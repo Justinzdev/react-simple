378b763fe50766c05b73d15cb8b42266fd2d67ee
JustinzDev <fullyz1532@gmail.com>
2023-08-03 18:18:25 +0700

    update get function

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,6 +6,7 @@ const Navbar = () => {
           <div className='fixed top-0 left-0 w-full list-none flex justify-center items-center gap-4 mt-5'>
                <Link to='/'><li>Main</li></Link>
                <Link to='/input'><li>Input</li></Link>
+               <Link to='/get'><li>Get</li></Link>
           </div>
      )
 }
